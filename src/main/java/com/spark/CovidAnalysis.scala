8b7c1bdd2e79ce87488451a68886f04f0c6a33f0
NarlaPavan <54428332+NarlaPavan@users.noreply.github.com>
2020-03-26 16:16:25 +0530

    covid analysis

--- src/main/java/com/spark/CovidAnalysis.scala
+++ src/main/java/com/spark/CovidAnalysis.scala
@@ -25,11 +25,12 @@ object CovidAnalysis {
     )
 
     val covid_df = spark.read.options(Map("header" -> "true", "inferschema" -> "true", "delimeter" -> ",")).csv("C:\\Users\\narla\\IdeaProjects\\comsparkscalapractise\\src\\main\\resources\\complete_data_new_format.csv")
-    covid_df.show(20)
-    val covid_df1 = covid_df.withColumn("Date",col("Date").cast(TimestampType))
+//    covid_df.show(20)
+    val covid_df1 = covid_df.withColumn("Date",to_timestamp(col("Date"),"MM/dd/yy"))
 
-//    covid_df1.groupBy(month(col("Date"))).agg(sum(col("Confirmed")) as "total confirmed",sum(col("Deaths")) as "Total Deaths").show()
-    covid_df1.printSchema()
-    covid_df1.show(20)
+    val covid_grp = covid_df1.groupBy(dayofmonth(col("Date")) as "day",month(col("Date")) as "month").agg(sum(col("Confirmed")) as "Total_Confirmed",sum(col("Deaths")) as "Total_Deaths").sort(col("month"),col("day"),col("Total_Confirmed"))
+    covid_grp.write.csv("C:\\Users\\narla\\Desktop\\covid_data")
+//    covid_df1.printSchema()
+//    covid_df1.show(20)
   }
 }
